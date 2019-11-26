506de20e4506d74b4f720daad2d5f701dc83146b
yeahjin0120 <55428346+yeahjin0120@users.noreply.github.com>
2019-11-26 15:34:38 +0900

    01_struct

--- main.c
+++ main.c
@@ -11,5 +11,13 @@ struct student{
 	};
 int main(int argc, char *argv[]) {
 	struct student s1 = { 1911141 , "½Å¿¹Áø" , 4.3};
+	s1.ID=123456;
+	strcpy(s1.name, "ye-jin");
+	s1.grade = 4.2;
+	
+	printf("ID : %d\n", s1.ID);
+	printf("name :%s\n", s1.name);
+	printf("grade :%f\n", s1.grade);
+	 
 	return 0;
 }
